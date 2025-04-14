dfd62eeff8fa39397aa4be34370e325cab4942a4
Your Name <you@example.com>
2025-04-14 17:54:14 +0300

    add global DataBase

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,5 @@
 
+<<<<<<< Updated upstream
 ﻿global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
 global using Microsoft.EntityFrameworkCore;
@@ -6,5 +7,14 @@ global using HandmadeMarket.Models;
 global using HandmadeMarket.DTO;
 global using HandmadeMarket.Interfaces;
 global using HandmadeMarket.Repository;
+=======
+﻿global using HandmadeMarket.Models;
+global using HandmadeMarket.DTO;
+global using HandmadeMarket.Interfaces;
+global using HandmadeMarket.Repository;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using Microsoft.EntityFrameworkCore;
+>>>>>>> Stashed changes
 global using HandmadeMarket.Context;
 

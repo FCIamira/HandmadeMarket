9030b3217bbb564e96beb28d3e85b980c6fd0222
Your Name <you@example.com>
2025-04-14 19:12:14 +0300

    add Shipment & add Customer

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -7,14 +7,6 @@ global using HandmadeMarket.Models;
 global using HandmadeMarket.DTO;
 global using HandmadeMarket.Interfaces;
 global using HandmadeMarket.Repository;
-=======
-﻿global using HandmadeMarket.Models;
-global using HandmadeMarket.DTO;
-global using HandmadeMarket.Interfaces;
-global using HandmadeMarket.Repository;
 global using System.ComponentModel.DataAnnotations;
-global using System.ComponentModel.DataAnnotations.Schema;
-global using Microsoft.EntityFrameworkCore;
->>>>>>> Stashed changes
 global using HandmadeMarket.Context;
 
---
+++ Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿namespace HandmadeMarket.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; } 
+
+        public virtual List<Shipment>? Shipments { get; set; }
+
+        public virtual List<Order_Item>? Orders { get; set; }
+
+        // public virtual List<Card>? Cards { get; set; }
+
+        //public virtual List<Wishlist>? Wishlist { get; set; }
+
+       // public virtual List<Payment>? Payment { get; set; }
+
+
+
+    }
+}

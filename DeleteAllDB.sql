09cb964919babd1400e0bec3b81c13c71f1c73fc
Your Name <you@example.com>
2025-04-15 16:58:15 +0300

    add Query to delete all data from db without structure

---
+++ DeleteAllDB.sql
@@ -0,0 +1,14 @@
+use db17344
+DELETE FROM Items;
+DELETE FROM Carts;
+DELETE FROM Wishlists;
+
+DELETE FROM [Orders];       
+DELETE FROM Shipments;
+
+DELETE FROM Products;
+
+DELETE FROM Customers;
+DELETE FROM Sellers;
+
+DELETE FROM Categories;
